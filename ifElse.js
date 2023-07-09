3b6743b57100760ab0afe534e38c68fc28b1ebdb
VanyaKalaijan0071711 <erneishin@gmail.com>
2023-07-09 16:02:07 +0600

    first commit

---
+++ ifElse.js
@@ -0,0 +1,209 @@
+// > - чон белгиси
+// < - кичинекей белги
+// >=- чон жанан барабар
+// <= - кичине же барабар
+// = - барабар
+// == - не строгий барабар
+// === - строгий барабар
+
+// console.log(21 > 20)
+// console.log(2 === 2)
+// console.log("2" == 2)
+// console.log("2"=== 2)
+// console.log(Number("2") === 2)
+
+// let str = "1234567"
+// console.log(Number(str))
+// console.log(+str)
+
+
+// let num = 10
+// console.log(num)
+// console.log(String(num))
+// console.log(num.toString(num))
+
+// if - эгерде
+// else - болбосо
+
+// if ( 5 === 5){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+// let num = 7
+// if (num >= 6){
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+
+// let color = "blue"
+//
+// if (color === "red"){
+//     console.log("Токто")
+// }else if(color === "yellow"){
+//     console.log("Даярдан")
+// }else if(color === "green"){
+//     console.log("айдасан болот")
+// }else{
+//     console.log("мындай цвет жок")
+// }
+
+
+// let a = 10
+// if (a === 10){
+//     console.log('Верно')
+// }else{
+//     console.log('Неверно')
+// }
+
+
+// let a = 50, b = 100
+// if (a < b){
+//     console.log('a больше b')
+// }else{
+//     console.log('a меньше b')
+// }
+
+// let a = 5
+// if (a > 0) {
+//     console.log('positive')
+// } else if (a === 0) {
+//     console.log('равно')
+// } else {
+//     console.log("negative")
+// }
+
+// let number = 45
+// if (number/2===0){
+//     console.log("Четное число")
+// }else {
+//     console.log("Нечетное число")
+// }
+
+// let a = 10
+// let b = 2
+// if (10 % 2 === 0) {
+//     console.log("Четное число")
+// } else {
+//     console.log("Нечетное число")
+// }
+// console.log(a % b === 0 ? "Четное число" : "Нечетное число")
+//
+// ? -> true
+// : -> false
+
+
+// let s = "wertyu"
+// if (s === "boolean"){
+//     console.log("boolean")
+// }else if (s === "number"){
+//     console.log("number")
+// }else if (s === "string" ){
+//     console.log("string")
+// }else {
+//     console.log("404")
+// }
+
+
+//let a = 5
+// console.log(a<9 ?'Верно' :'Неверно')
+
+
+// let a= 3
+// if(a === 3||a === 7 ){
+//     console.log(a + 7)
+// }else{
+//     console.log(a / 10)
+// }
+
+
+// console.log(false && true || true && true)
+
+
+// alert()
+
+// alert("HEllO")
+
+// let day = 1
+// if (day ===1){
+//     console.log("Понедельник")
+// }else if(day ===2){
+//     console.log("Вторник")
+// }else if (day ===3){
+//     console.log("Среда"){
+
+
+//let month = prompt()
+//if (month === "3"){
+//console.log("spring")
+// } else if (month === "6"){
+//console.log("summer")
+//}else if (month === "9"){
+//console.log("autumn")
+//}else if (month ==="11"){
+//console.log("winter")
+// }
+
+
+
+// alert ()
+
+// let color = prompt()
+// if (color === "red") {
+//     console.log("токто")
+// } else if (color === "green") {
+//     console.log("отсо болот")
+// } else if (color === "yellow") {
+//     console.log("даярдан")
+// } else if (color === "grey") {
+//     console.log(" 404")
+// } else if (color === "blue") {
+//     console.log(" туура эмес")
+// }
+
+
+//
+// let film =prompt()
+// if (film >= 1 && film <= 6){
+//     console.log("Настя и Папа")
+// }else if (film >= 7 && film <=12){
+//     console.log("Дикий кот")
+// }else if (film >= 13 && film <=17){
+//     console.log("Полезные курсы")
+// }else if (film >=18 && film <=50){
+//     console.log("Бизнес с нуля")
+// }else {
+//     console.log("Как не попасть в дом для присторелых?")
+// }
+
+// alert()
+//
+// let month = +prompt()
+// if (month >= 12&& month <=2){
+//     console.log("Зима")
+// }else if (month >=3 &&month<=5){
+//     console.log("Весна")
+// } else if (month >=6 && month <=8){
+//     console.log("Лето")
+// }else if (month<=9 && month>=11){
+//     console.log("Осень")
+// }
+
+//let week = prompt()
+// if ( week ==="понедельник"){
+//     console.log("1")
+// } else if (week === "вторник"){
+//     console.log("2")
+// } else if (week === "среда"){
+//     console.log("3")
+// } else if (week === "четверг"){
+//     console.log("4")
+// } else if (week === "пятница"){
+//     console.log("5")
+// } else if (week === "субота"){
+//     console.log("6")
+// } else if (week === "воскресенье"){
+//     console.log("7")
+// }
+
